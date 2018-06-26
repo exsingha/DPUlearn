ae5a9af241c37a76c35ced1711854083e9017bd5
phudit kittimongkk <exsingha@gmail.com>
2018-06-26 12:14:44 +0700

    update lab08_01 final

--- Lab08_01/Lab08_01/Program.cs
+++ Lab08_01/Lab08_01/Program.cs
@@ -9,46 +9,88 @@ namespace Lab08_01
 {
     class Program
     {
+        public static VendingMachine coke = new VendingMachine();
+        public static VendingMachine water = new VendingMachine();
+        public static VendingMachine greentea = new VendingMachine();
 
         static void Main(string[] args)
         {
-            //string path = Directory.GetCurrentDirectory();
             try
             {
-                string path = "C:/Users/DPU/source/repos/DPUlearn/Lab08_01/chapter8.txt";
-                string newpath = "C:/Users/DPU/source/repos/DPUlearn/Lab08_01/chapter8copy.txt";
-                writeFile(path);
-                readFile(path);
+                //string path = "C:/Users/DPU/source/repos/DPUlearn/Lab08_01/chapter8.txt";
+                //string newpath = "C:/Users/DPU/source/repos/DPUlearn/Lab08_01/chapter8copy.txt";
+                string coke_path = "C:/Users/DPU/source/repos/DPUlearn/Lab08_01/coke.txt";
+                string water_path = "C:/Users/DPU/source/repos/DPUlearn/Lab08_01/water.txt";
+                string greentea_path = "C:/Users/DPU/source/repos/DPUlearn/Lab08_01/greentea.txt";
 
-                copyFile(path,newpath);
+                int num = 0;
+                int num2 = 0;
+
+                //writeFile(path);
+                //readFile(path);
+                //copyFile(path,newpath);
+
+
+
+                coke.SetPrice(15);
+                coke.SetSize(100);
+
+                water.SetPrice(10);
+                water.SetSize(100);
+
+                greentea.SetPrice(12);
+                greentea.SetSize(100);
+
+                Console.WriteLine(":: Choose type ::");
+                Console.WriteLine("1. Coke \t 2. Water \t 3. Green Tea");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Size is ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                switch (num)
+                {
+                    case 1: coke.SetSize(num2); break;
+                    case 2: water.SetSize(num2); break;
+                    case 3: greentea.SetSize(num2); break;
+                    default: Console.WriteLine("Error"); break;
+                }
+                writeFile(coke_path, coke);
+                writeFile(water_path, water);
+                writeFile(greentea_path, greentea);
+
+                readFile(coke_path);
+                readFile(water_path);
+                readFile(greentea_path);
+
+                Console.WriteLine();
             }
             catch (IOException)
             {
                 Console.WriteLine("Error");
             }
-            //finally
-            //{
-            //    Console.WriteLine("Error other!");
-            //}
 
+        }
+        static void writeFile(string filename, VendingMachine vm)
+        {
+            StreamWriter sw = new StreamWriter(filename, false, Encoding.Unicode);
+
+            sw.WriteLine(vm.GetPrice());
+            sw.WriteLine(vm.GetSize());
+
+            sw.Close();
         }
         static void writeFile(string filename)
         {
-            StreamWriter sw = new StreamWriter(filename, true, Encoding.Unicode);
-            sw.WriteLine("DIT");
-            sw.WriteLine("Yu");
-            sw.WriteLine("PED");
+            StreamWriter sw = new StreamWriter(filename, false, Encoding.Unicode);
+            
+            //sw.WriteLine("DIT");
+            //sw.WriteLine("Yu");
+            //sw.WriteLine("PED");
+
             sw.Close();
         }
-        //static void writeFile(string filename,string line)
-        //{
-        //    StreamWriter sw = new StreamWriter(filename, true, Encoding.Unicode);
-        //    do
-        //    {
-        //        sw.WriteLine(line);
-        //    } while (line != null);
-        //    sw.Close();
-        //}
+
         static void readFile(string filename)
         {
             string line = "";
@@ -64,11 +106,12 @@ namespace Lab08_01
 
             sr.Close();
         }
+
         static void copyFile(string source, string destination)
         {
             string line = "";
             StreamReader sr = new StreamReader(source);
-            StreamWriter sw = new StreamWriter(destination, true, Encoding.Unicode);
+            StreamWriter sw = new StreamWriter(destination, false, Encoding.Unicode);
             do
             {
                 Console.WriteLine(line);
---
+++ Lab08_01/Lab08_01/VendingMachine.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08_01
+{
+    class VendingMachine
+    {
+        private int coke_size;
+        private int water_size;
+        private int greeTea_size;
+        private int size;
+        private int price;
+        private int money;
+
+        public VendingMachine()
+        {
+            coke_size = 0;
+            water_size = 0;
+            greeTea_size = 0;
+            size = 0;
+            price = 0;
+            money = 0;
+        }
+        public void SetSize(int size)
+        {
+            this.size = size;
+        }
+
+        public int GetSize()
+        {
+            return size;
+        }
+
+        public void SetPrice(int price)
+        {
+            this.price = price;
+        }
+
+        public int GetPrice()
+        {
+            return price;
+        }
+        public int SellProduct(int money)
+        {
+            this.money = money;
+            return money - price;
+        }
+    }
+}
