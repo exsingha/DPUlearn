19c6982bbd88b483a3a4db11b7e8d561eae6ce2e
phudit kittimongkk <exsingha@gmail.com>
2018-06-26 13:46:50 +0700

    add lecture OOP 9

---
+++ OOPLecture09/OOPLecture09/LivingThing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLecture09
+{
+    abstract class LivingThing
+    {
+        public void breath()
+        {
+            Console.WriteLine("Living Thing breathing...");
+        }
+        public void eat()
+        {
+            Console.WriteLine("Living Thing eating...");
+        }
+        public abstract void walk();
+    }
+}
